2af11eceb658d39923b5e23efb5ddb78b56cf4b6
Logan Garverick <logan.garverick@gmail.com>
2022-04-09 15:35:26 -0400

    DEV: Implement PE32 file header parsing (2/?)

--- src/PE32.py
+++ src/PE32.py
@@ -8,6 +8,8 @@ import struct
 
 E_RES1_SIZE = 0x8
 E_RES2_SIZE = 0x14
+IMAGE_FILE_HEADER_OFFSET = 0x4
+IMAGE_OPTIONAL_HEADER_OFFSET = 0x18
 
 
 class PE32(BinaryFile):
@@ -15,13 +17,17 @@ class PE32(BinaryFile):
 
     def __init__(self, path):
         self.path = path
-        self.self.formatDict = None
+        self.formatDict = None
         self._find_endianess()
-        self._IMAGE_DOS_HEADER = self._read_DOS_header()
+        self._IMAGE_DOS_HEADER = self._read_IMAGE_DOS_HEADER()
+        self._IMAGE_FILE_HEADER = self._read_IMAGE_FILE_HEADER()
+        self._IMAGE_OPTIONAL_HEADER = self._read_IMAGE_OPTIONAL_HEADER()
 
     def _find_endianess(self) -> None:
         """All windows PE formats are assumed to be compiled in Little Endian format"""
-        self.self.formatDict = {
+        self.formatDict = {
+            "BYTE_F": "<B",
+            "BYTE_S": 1,
             "WORD_F": "<H",
             "WORD_S": 2,
             "DWORD_F": "<L",
@@ -30,7 +36,7 @@ class PE32(BinaryFile):
             "QWORD_S": 8,
         }
 
-    def _read_DOS_header(self) -> dict:
+    def _read_IMAGE_DOS_HEADER(self) -> dict:
 
         _IMAGE_DOS_HEADER = {}
         with open(self.path, "rb") as file:
@@ -79,20 +85,151 @@ class PE32(BinaryFile):
             (_IMAGE_DOS_HEADER["e_cblp"],) = struct.unpack(
                 self.formatDict["WORD_F"], file.read(self.formatDict["WORD_S"])
             )
-
             # Skip reserved word section 1
             file.seek(E_RES1_SIZE, 1)
-
             (_IMAGE_DOS_HEADER["e_oemid"],) = struct.unpack(
                 self.formatDict["WORD_F"], file.read(self.formatDict["WORD_S"])
             )
             (_IMAGE_DOS_HEADER["e_oeminfo"],) = struct.unpack(
                 self.formatDict["WORD_F"], file.read(self.formatDict["WORD_S"])
             )
-
             # Skip reserved word section 2
             file.seek(E_RES1_SIZE, 1)
-
             (_IMAGE_DOS_HEADER["e_lfanew"],) = struct.unpack(
                 self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
             )
+
+            return _IMAGE_DOS_HEADER
+
+    def _read_IMAGE_FILE_HEADER(self) -> dict:
+        _IMAGE_FILE_HEADER = {}
+        with open(self.path, "rb") as file:
+            # Jump to the _IMAGE_FILE_HEADER struct
+            file.seek(self._IMAGE_DOS_HEADER["e_lfanew"], 0)
+            file.seek(IMAGE_FILE_HEADER_OFFSET, 1)
+
+            # Parse _IMAGE_FILE_HEADER struct
+            (_IMAGE_FILE_HEADER["Machine"],) = struct.unpack(
+                self.formatDict["WORD_F"], file.read(self.formatDict["WORD_S"])
+            )
+            (_IMAGE_FILE_HEADER["NumberOfSections"],) = struct.unpack(
+                self.formatDict["WORD_F"], file.read(self.formatDict["WORD_S"])
+            )
+            (_IMAGE_FILE_HEADER["TimeDateStamp"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_FILE_HEADER["PointerToSymbolTable"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_FILE_HEADER["NumberOfSymbols"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_FILE_HEADER["SizeOfOptionalHeader"],) = struct.unpack(
+                self.formatDict["WORD_F"], file.read(self.formatDict["WORD_S"])
+            )
+            (_IMAGE_FILE_HEADER["Characteristics"],) = struct.unpack(
+                self.formatDict["WORD_F"], file.read(self.formatDict["WORD_S"])
+            )
+
+            return _IMAGE_FILE_HEADER
+
+    def _read_IMAGE_OPTIONAL_HEADER(self) -> dict:
+        _IMAGE_OPTIONAL_HEADER = {}
+        with open(self.path, "rb") as file:
+            # Jump to the _IMAGE_OPTIONAL_HEADER struct
+            file.seek(self._IMAGE_DOS_HEADER["e_lfanew"], 0)
+            file.seek(IMAGE_OPTIONAL_HEADER_OFFSET, 1)
+
+            # Parse _IMAGE_OPTIONAL_HEADER struct
+            (_IMAGE_OPTIONAL_HEADER["Magic"],) = struct.unpack(
+                self.formatDict["WORD_F"], file.read(self.formatDict["WORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["MajorLinkerVersion"],) = struct.unpack(
+                self.formatDict["BYTE_F"], file.read(self.formatDict["BYTE_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["MinorLinkerVersion"],) = struct.unpack(
+                self.formatDict["BYTE_F"], file.read(self.formatDict["BYTE_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["SizeOfCode"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["SizeOfInitializedData"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["SizeOfUninitializedData"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["AddressOfEntryPoint"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["BaseOfCode"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["BaseOfData"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["ImageBase"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["SectionAlignment"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["FileAlignment"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["MajorOperatingSystemVersion"],) = struct.unpack(
+                self.formatDict["WORD_F"], file.read(self.formatDict["WORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["MinorOperatingSystemVersion"],) = struct.unpack(
+                self.formatDict["WORD_F"], file.read(self.formatDict["WORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["MajorImageVersion"],) = struct.unpack(
+                self.formatDict["WORD_F"], file.read(self.formatDict["WORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["MinorImageVersion"],) = struct.unpack(
+                self.formatDict["WORD_F"], file.read(self.formatDict["WORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["MajorSubsystemVersion"],) = struct.unpack(
+                self.formatDict["WORD_F"], file.read(self.formatDict["WORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["MinorSubsystemVersion"],) = struct.unpack(
+                self.formatDict["WORD_F"], file.read(self.formatDict["WORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["Win32VersionValue"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["SizeOfImage"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["SizeOfHeaders"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["Checksum"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["Subsystem"],) = struct.unpack(
+                self.formatDict["WORD_F"], file.read(self.formatDict["WORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["DllCharacteristics"],) = struct.unpack(
+                self.formatDict["WORD_F"], file.read(self.formatDict["WORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["SizeOfStackReserve"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["SizeOfStackCommit"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["SizeOfHeapReserve"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["SizeOfHeapCommit"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["LoaderFlags"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+            (_IMAGE_OPTIONAL_HEADER["NumberOfRvaAndSizes"],) = struct.unpack(
+                self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+            )
+
+            return _IMAGE_OPTIONAL_HEADER
