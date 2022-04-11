4a26b9f64483a92a6b7c89459fbd8fe2bedbc714
Logan Garverick <logan.garverick@gmail.com>
2022-04-10 22:04:48 -0400

    DEV: Implement PE32 file header parsing (5/?)

--- src/PE32.py
+++ src/PE32.py
@@ -10,6 +10,24 @@ E_RES1_SIZE = 0x8
 E_RES2_SIZE = 0x14
 IMAGE_FILE_HEADER_OFFSET = 0x4
 IMAGE_OPTIONAL_HEADER_OFFSET = 0x18
+IMAGE_DATA_DIRECTORY_TABLE_OFFSET = 0x78
+IMAGE_DATA_DIRECTORY_DICT = [
+    "IMAGE_DIRECTORY_ENTRY_EXPORT",
+    "IMAGE_DIRECTORY_ENTRY_IMPORT",
+    "IMAGE_DIRECTORY_ENTRY_RESOURCE",
+    "IMAGE_DIRECTORY_ENTRY_EXCEPTION",
+    "IMAGE_DIRECTORY_ENTRY_SECURITY",
+    "IMAGE_DIRECTORY_ENTRY_BASERELOC",
+    "IMAGE_DIRECTORY_ENTRY_DEBUG",
+    "IMAGE_DIRECTORY_ENTRY_COPYRIGHT",
+    "IMAGE_DIRECTORY_ENTRY_GLOBALPTR",
+    "IMAGE_DIRECTORY_ENTRY_TLS",
+    "IMAGE_DIRECTORY_ENTRY_LOAD_CONFIG",
+    "IMAGE_DIRECTORY_ENTRY_BOUND_IMPORT",
+    "IMAGE_DIRECTORY_ENTRY_IAT",
+    "IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT",
+    "IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR",
+]
 IMAGE_DIRECTORY_ENTRY_IMPORT_OFFSET = 0x80
 IMAGE_DIRECTORY_ENTRY_TLS_OFFSET = 0xB8
 IMAGE_DIRECTORY_ENTRY_IAT_OFFSET = 0xD0
@@ -30,6 +48,7 @@ class PE32(BinaryFile):
         self._IMAGE_DOS_HEADER = self._read_IMAGE_DOS_HEADER()
         self._IMAGE_FILE_HEADER = self._read_IMAGE_FILE_HEADER()
         self._IMAGE_OPTIONAL_HEADER = self._read_IMAGE_OPTIONAL_HEADER()
+        self._IMAGE_DATA_DIRECTORY_TABLE = self._read_IMAGE_DATA_DIRECTORY_array()
         self._IMAGE_DIRECTORY_ENTRY_IMPORT = self._read_IMAGE_DIRECTORY_ENTRY_IMPORT()
         self._IMAGE_DIRECTORY_ENTRY_TLS = self._read_IMAGE_DIRECTORY_ENTRY_TLS()
 
@@ -37,22 +56,22 @@ class PE32(BinaryFile):
         print(
             f"DOS HEADER (_IMAGE_DOS_HEADER):\n"
             + f"    Magic Number:\t\t\t{hex(self._IMAGE_DOS_HEADER['e_magic'])} ({self._IMAGE_DOS_HEADER['e_magic_characters']})\n"
-            + f"    Bytes in last page:\t\t\t{self._IMAGE_DOS_HEADER['e_cblp']}\n"
-            + f"    Pages in file:\t\t\t{self._IMAGE_DOS_HEADER['e_cp']}\n"
+            + f"    Bytes in Last Page:\t\t\t{self._IMAGE_DOS_HEADER['e_cblp']}\n"
+            + f"    Pages in File:\t\t\t{self._IMAGE_DOS_HEADER['e_cp']}\n"
             + f"    Relocations:\t\t\t{self._IMAGE_DOS_HEADER['e_crlc']}\n"
-            + f"    Size of header in paragraphs:\t{self._IMAGE_DOS_HEADER['e_cparhdr']}\n"
-            + f"    Minimum extra paragraphs:\t\t{self._IMAGE_DOS_HEADER['e_minalloc']}\n"
-            + f"    Maximum extra paragraphs:\t\t{self._IMAGE_DOS_HEADER['e_maxalloc']}\n"
-            + f"    Initial (relative) SS value:\t{self._IMAGE_DOS_HEADER['e_ss']}\n"
-            + f"    Initial SP value:\t\t\t{hex(self._IMAGE_DOS_HEADER['e_sp'])}\n"
+            + f"    Size of Header in Paragraphs:\t{self._IMAGE_DOS_HEADER['e_cparhdr']}\n"
+            + f"    Minimum Extra Paragraphs:\t\t{self._IMAGE_DOS_HEADER['e_minalloc']}\n"
+            + f"    Maximum Extra Paragraphs:\t\t{self._IMAGE_DOS_HEADER['e_maxalloc']}\n"
+            + f"    Initial (relative) SS Value:\t{self._IMAGE_DOS_HEADER['e_ss']}\n"
+            + f"    Initial SP Value:\t\t\t{hex(self._IMAGE_DOS_HEADER['e_sp'])}\n"
             + f"    Checksum:\t\t\t\t{self._IMAGE_DOS_HEADER['e_csum']}\n"
-            + f"    Initial IP value:\t\t\t{self._IMAGE_DOS_HEADER['e_ip']}\n"
-            + f"    Initial (relative) CS value:\t{self._IMAGE_DOS_HEADER['e_cs']}\n"
-            + f"    File address of relocation table:\t{hex(self._IMAGE_DOS_HEADER['e_lfarlc'])}\n"
-            + f"    Overlay number:\t\t\t{self._IMAGE_DOS_HEADER['e_ovno']}\n"
+            + f"    Initial IP Value:\t\t\t{self._IMAGE_DOS_HEADER['e_ip']}\n"
+            + f"    Initial (relative) CS Value:\t{self._IMAGE_DOS_HEADER['e_cs']}\n"
+            + f"    File Address of relocation table:\t{hex(self._IMAGE_DOS_HEADER['e_lfarlc'])}\n"
+            + f"    Overlay Number:\t\t\t{self._IMAGE_DOS_HEADER['e_ovno']}\n"
             + f"    OEM Identifier:\t\t\t{self._IMAGE_DOS_HEADER['e_oemid']}\n"
             + f"    OEM Information:\t\t\t{self._IMAGE_DOS_HEADER['e_oeminfo']}\n"
-            + f"    File address of new exe header:\t{hex(self._IMAGE_DOS_HEADER['e_lfanew'])}\n"
+            + f"    File Address of New Exe Header:\t{hex(self._IMAGE_DOS_HEADER['e_lfanew'])}\n"
             + f"\n"
             + f"COFF/FILE HEADER (_IMAGE_FILE_HEADER):\n"
             + f"    Machine:\t\t\t\t{hex(self._IMAGE_FILE_HEADER['Machine'])} ({self._IMAGE_FILE_HEADER['MachineName']})\n"
@@ -62,7 +81,44 @@ class PE32(BinaryFile):
             + f"    Number of Symbols:\t\t\t{self._IMAGE_FILE_HEADER['NumberOfSymbols']}\n"
             + f"    Size of Optional Header:\t\t{hex(self._IMAGE_FILE_HEADER['SizeOfOptionalHeader'])}\n"
             + f"    Characteristics:\t\t\t{hex(self._IMAGE_FILE_HEADER['Characteristics'])}\n"
+            + f"\n"
+            + f"OPTIONAL HEADER (_IMAGE_OPTIONAL_HEADER):\n"
+            + f"    Magic:\t\t\t\t{self._IMAGE_OPTIONAL_HEADER['Magic']}\n"
+            + f"    Linker Version:\t\t\t{self._IMAGE_OPTIONAL_HEADER['MajorLinkerVersion']}.{self._IMAGE_OPTIONAL_HEADER['MinorLinkerVersion']}\n"
+            + f"    Size of Code:\t\t\t{hex(self._IMAGE_OPTIONAL_HEADER['SizeOfCode'])}\n"
+            + f"    Size of Initialized Data:\t\t{hex(self._IMAGE_OPTIONAL_HEADER['SizeOfInitializedData'])}\n"
+            + f"    Size of Uninitialized Data:\t\t{hex(self._IMAGE_OPTIONAL_HEADER['SizeOfUninitializedData'])}\n"
+            + f"    Address of Entry Point:\t\t{hex(self._IMAGE_OPTIONAL_HEADER['AddressOfEntryPoint'])}\n"
+            + f"    Base of Code:\t\t\t{hex(self._IMAGE_OPTIONAL_HEADER['BaseOfCode'])}\n"
+            + f"    Base of Data:\t\t\t{hex(self._IMAGE_OPTIONAL_HEADER['BaseOfData'])}\n"
+            + f"    Image Base:\t\t\t\t{hex(self._IMAGE_OPTIONAL_HEADER['ImageBase'])}\n"
+            + f"    Section Alignment:\t\t\t{hex(self._IMAGE_OPTIONAL_HEADER['SectionAlignment'])}\n"
+            + f"    File Alignment:\t\t\t{hex(self._IMAGE_OPTIONAL_HEADER['FileAlignment'])}\n"
+            + f"    Operating System Version:\t\t{self._IMAGE_OPTIONAL_HEADER['MajorOperatingSystemVersion']}.{self._IMAGE_OPTIONAL_HEADER['MinorOperatingSystemVersion']}\n"
+            + f"    Image Version:\t\t\t{self._IMAGE_OPTIONAL_HEADER['MajorImageVersion']}.{self._IMAGE_OPTIONAL_HEADER['MinorImageVersion']}\n"
+            + f"    Subsystem Version:\t\t\t{self._IMAGE_OPTIONAL_HEADER['MajorSubsystemVersion']}.{self._IMAGE_OPTIONAL_HEADER['MinorSubsystemVersion']}\n"
+            + f"    Win32 Version Value:\t\t{self._IMAGE_OPTIONAL_HEADER['Win32VersionValue']}\n"
+            + f"    Size of Image:\t\t\t{hex(self._IMAGE_OPTIONAL_HEADER['SizeOfImage'])}\n"
+            + f"    Size of Headers:\t\t\t{hex(self._IMAGE_OPTIONAL_HEADER['SizeOfHeaders'])}\n"
+            + f"    Checksum:\t\t\t\t{hex(self._IMAGE_OPTIONAL_HEADER['Checksum'])}\n"
+            + f"    Subsystem:\t\t\t\t{self._IMAGE_OPTIONAL_HEADER['Subsystem']}\n"
+            + f"    DLL Characteristics:\t\t{hex(self._IMAGE_OPTIONAL_HEADER['DllCharacteristics'])}\n"
+            + f"    Size of Stack Reserve:\t\t{hex(self._IMAGE_OPTIONAL_HEADER['SizeOfStackReserve'])}\n"
+            + f"    Size of Stack Commit:\t\t{hex(self._IMAGE_OPTIONAL_HEADER['SizeOfStackCommit'])}\n"
+            + f"    Size of Heap Reserve:\t\t{hex(self._IMAGE_OPTIONAL_HEADER['SizeOfHeapReserve'])}\n"
+            + f"    Sizeof Heap Commit:\t\t\t{hex(self._IMAGE_OPTIONAL_HEADER['SizeOfHeapCommit'])}\n"
+            + f"    Loader Flags:\t\t\t{hex(self._IMAGE_OPTIONAL_HEADER['LoaderFlags'])}\n"
+            + f"    Number of RVA and Sizes:\t\t{hex(self._IMAGE_OPTIONAL_HEADER['NumberOfRvaAndSizes'])}\n"
+            + f"\n"
+            + f"DATA DIRECTORIES (_IMAGE_OPTIONAL_HEADER):"
         )
+        # Create a list of DATA_DIRECTORY names from dictionary keys
+        DATA_DIRECTORIES = list(self._IMAGE_DATA_DIRECTORY_TABLE.keys())
+        for DATA_DIRECTORY in DATA_DIRECTORIES:
+            infoDict = self._IMAGE_DATA_DIRECTORY_TABLE[DATA_DIRECTORY]
+            print(
+                f"    {DATA_DIRECTORY:<40}  {hex(infoDict['VirtualAddress'])} (Length: {hex(infoDict['Size'])})"
+            )
 
     def _find_endianess(self) -> None:
         """All windows PE formats are assumed to be compiled in Little Endian format"""
@@ -279,6 +335,37 @@ class PE32(BinaryFile):
             )
 
             return _IMAGE_OPTIONAL_HEADER
+
+    def _read_IMAGE_DATA_DIRECTORY_array(self) -> list:
+        _IMAGE_DATA_DIRECTORY_TABLE = {}
+        with open(self.path, "rb") as file:
+            # Jump to the _IMAGE_DATA_DIRECTORY table at the end of the _IMAGE_OPTIONAL_HEADER struct
+            file.seek(self._IMAGE_DOS_HEADER["e_lfanew"], 0)
+            file.seek(IMAGE_DATA_DIRECTORY_TABLE_OFFSET, 1)
+
+            # Parse the _IMAGE_DATA_DIRECTORY table (NULL entries indicate that the data directory is not present)
+            for IMAGE_DATA_DIRECTORY in IMAGE_DATA_DIRECTORY_DICT:
+                DATA_DIRECTORY_ENTRY = {}
+                (DATA_DIRECTORY_ENTRY["VirtualAddress"],) = struct.unpack(
+                    self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+                )
+                (DATA_DIRECTORY_ENTRY["Size"],) = struct.unpack(
+                    self.formatDict["DWORD_F"], file.read(self.formatDict["DWORD_S"])
+                )
+
+                if (
+                    DATA_DIRECTORY_ENTRY["VirtualAddress"] != 0
+                    and DATA_DIRECTORY_ENTRY["Size"] != 0
+                ):
+                    # If the _IMAGE_DATA_DIRECTORY entry exists, add it to the dictionary of data directories
+                    _IMAGE_DATA_DIRECTORY_TABLE[
+                        IMAGE_DATA_DIRECTORY
+                    ] = DATA_DIRECTORY_ENTRY
+                else:
+                    # If the _IMAGE_DATA_DIRECTORY entry is empty, the directory does not exist in the binary
+                    pass
+
+            return _IMAGE_DATA_DIRECTORY_TABLE
 
     def _read_IMAGE_DIRECTORY_ENTRY_IMPORT(self) -> dict:
         IMAGE_DIRECTORY_ENTRY_IMPORT = {}
